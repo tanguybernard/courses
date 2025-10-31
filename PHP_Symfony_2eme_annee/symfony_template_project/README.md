259a3462fc68d3f10dc54a0fae5514a442a3ff42
Tanguy Bernard <tanguy.bernard@shodo-studio.io>
2025-10-31 09:23:42 +0100

    feat(Symfony): add admin section with role-based access and dynamic redirect handling

--- PHP_Symfony_2eme_annee/symfony_template_project/README.md
+++ PHP_Symfony_2eme_annee/symfony_template_project/README.md
@@ -24,3 +24,26 @@ Register
 
 
 Pour tester : mail shodo, password : admin1234
+
+
+### Créer un utilisateur Admin
+
+    php bin/console security:hash-password
+
+➡️ Saisis un mot de passe, récupère le hash
+
+Puis :
+
+
+    php bin/console doctrine:query:sql "INSERT INTO user (email, roles, password) VALUES ('admin@example.com', '["ROLE_ADMIN"]', '<LE_HASH>');"
+
+
+Dans Docker :
+
+Attention dans Docker  le hash, on doit echapper les caractères spéciaux  pour indiquer au système qu’il doit être interprété comme un caractère normal.
+
+Exemple ici : \$2y\$13\$
+
+    docker-compose run --rm php bin/console doctrine:query:sql "INSERT INTO user (email, roles, password) VALUES ('admin@example.com', '[\"ROLE_ADMIN\"]', '\$2y\$13\$7VBGx0Ti2hVpGlHA9/eDReYpurXp97pM5l7TvxudcrABKh3g9sLyG');"
+
+Pour tester : admin@example.com , password : admin1234
---
+++ PHP_Symfony_2eme_annee/symfony_template_project/app/src/Security/LoginSuccessHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Routing\RouterInterface;
+
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    private RouterInterface $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): RedirectResponse
+    {
+        $roles = $token->getRoleNames();
+
+        if (in_array('ROLE_ADMIN', $roles, true)) {
+            $redirectUrl = $this->router->generate('app_admin');
+        } else {
+            $redirectUrl = $this->router->generate('app_dashboard');
+        }
+
+        return new RedirectResponse($redirectUrl);
+    }
+}
